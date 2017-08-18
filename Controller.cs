ae06bd806ae98c063affa06ea1544a97a73caeb9
Mark Pavlovski <markpavlovski@gmail.com>
2017-08-18 07:58:10 -0700

    Added a game controller object

---
+++ Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public Initialize prefab;
+
+	Initialize newRound;
+
+
+	void Start(){
+
+		newRound = Instantiate<Initialize> (prefab);
+
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			
+			Destroy(newRound);
+
+		}
+	}
+}
